6870048040f3573de04ac7446786c5fe17b20e71
Maniteja Gajminkar <maniteja.gajminkar25@gmail.com>
2025-07-12 17:12:43 +0530

    basicssss

---
+++ rough.py
@@ -0,0 +1,5 @@
+from oops_proj import chatbook
+
+user1 = chatbook()
+#Can the object be named user1, even if it was named obj in oops_proj.py?
+#✅ Yes — object names are local to the file (script).
